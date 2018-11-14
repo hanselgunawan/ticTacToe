52ce284b8654865aa295bea307c57e022b21160d
Hansel Tritama <hanseltritama@lmhanselt05283.home>
2018-11-14 00:46:41 -0800

    added spaces, removed unnecessary logic, and added validation.

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 public class TicTacToe {
 
-  public static boolean checkCol(int coord, int[][] board) {
-    return (board[0][coord] == board[1][coord]) && (board[1][coord] == board[2][coord]);
+  public static boolean checkCol(int x_coord, int[][] board) {
+    return (board[0][x_coord] == board[1][x_coord]) && (board[1][x_coord] == board[2][x_coord]);
   }
 
-  public static boolean checkRow(int coord, int[][] board) {
-    return (board[coord][0] == board[coord][1]) && (board[coord][1] == board[coord][2]);
+  public static boolean checkRow(int y_coord, int[][] board) {
+    return (board[y_coord][0] == board[y_coord][1]) && (board[y_coord][1] == board[y_coord][2]);
   }
 
-  public static boolean checkRightDiagonal(int[][] board) {
-    return (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]);
+  public static boolean checkRightDiagonal(int x_coord, int y_coord, int[][] board) {
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
+    else if(x_coord != y_coord) return false;
   }
 
-  public static boolean checkLeftDiagonal(int[][] board) {
-    return (board[0][2] == board[1][1]) && (board[1][1] == board[2][0]);
+  public static boolean checkLeftDiagonal(int x_coord, int y_coord, int[][] board) {
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true;
+    else if(x_coord != 2 - y_coord) return false;
   }
 
   public static void main(String[] args) {
@@ -23,50 +25,28 @@ public class TicTacToe {
     int turn = 0;
     int winner = 0;
 
-    while(turn<9 && winner == 0) {
-      int player_turn = turn%2+1;
+    while(turn < 9 && winner == 0) {
+      int player_turn = turn % 2 + 1;
       Scanner sc = new Scanner(System.in);
-      System.out.print("Player "+ player_turn + ": ");
+      System.out.print("Player " + player_turn + ": ");
 
       String[] user_input = sc.nextLine().split(",");
       int x_coord = Integer.parseInt(user_input[0]);
       int y_coord = Integer.parseInt(user_input[1]);
 
-      board[y_coord][x_coord] = player_turn;
-
-      if(turn >= 4) {
-          if(y_coord == x_coord) {
-            if(y_coord == 1 && x_coord == 1) {
-              if(checkCol(x_coord, board) || checkRow(y_coord, board) ||
-                checkRightDiagonal(board) || checkLeftDiagonal(board)) {
-                winner = player_turn;
-                System.out.println("Player " + winner + " Wins!");
-              }
-            }
-            else
-            {
-              if(checkCol(x_coord, board) || checkRow(y_coord, board) ||
-                checkRightDiagonal(board)) {
-                winner = player_turn;
-                System.out.println("Player " + winner + " Wins!");
-            }
-          }
-        } else if ((y_coord == 0 && x_coord == board[0].length-1) || (y_coord == board[0].length-1 && x_coord == 0)) {
-          if(checkCol(x_coord, board) || checkRow(y_coord, board) ||
-            checkLeftDiagonal(board)) {
-            winner = player_turn;
-            System.out.println("Player " + winner + " Wins!");
-          }
-        } else {
-          if(checkCol(x_coord, board) || checkRow(y_coord, board)) {
-            winner = player_turn;
-            System.out.println("Player " + winner + " Wins!");
-          }
-        }
+      if(board[y_coord][x_coord] == 0) {
+        board[y_coord][x_coord] = player_turn;
+        turn++;
+      } else {
+        System.out.println("That place is chosen by other player. Try other place.");
+        continue;
       }
 
-      turn++;
+    if(turn >= 4 && (checkRow(y_coord, board) || checkCol(x_coord, board) || checkLeftDiagonal(x_coord, y_coord, board) || checkRightDiagonal(x_coord, y_coord, board))) {
+        winner = player_turn;
+        System.out.println("Player " + winner + " Wins!");
+      }
     }
+    if(turn == 8) System.out.println("Tie!");
   }
 }
-
